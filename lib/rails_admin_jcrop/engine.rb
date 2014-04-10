2a1f7a56469eb58ba90a64478b3698000cc09595
Ben Lovell <benjamin.lovell@gmail.com>
2014-04-10 16:00:17 +0100

    Changes to accomodate cropping in CMS

--- lib/rails_admin_jcrop/engine.rb
+++ lib/rails_admin_jcrop/engine.rb
@@ -1,7 +1,5 @@
 module RailsAdminJcrop
   class Engine < ::Rails::Engine
-    isolate_namespace RailsAdmin
-
     initializer "RailsAdminJcrop precompile hook" do |app|
       app.config.assets.precompile += ['rails_admin/ra.jcrop.js', 'rails_admin/jquery.Jcrop.js', 'rails_admin/jquery.Jcrop.css']
     end
